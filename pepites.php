5ca4ffffe370c608e84782f1a0923d9cc4f07e1a
VitaBar7 <marabar@gmail.com>
2022-10-26 22:34:11 +0200

    navbar active link wip

--- pepites.php
+++ pepites.php
@@ -7,7 +7,21 @@ $reponse = $bdd->query("SELECT * FROM girls_rock");
 
 <?php 
 include 'head.php'; 
-include 'navbar.php'; ?>
+?>
+<nav class="navbar sticky-top navbar-expand-lg navbar-light nav_txt" style="background-color: rgba(23, 217, 136, 0.7);">
+  <a class="navbar-brand" href="index.php"><img src="https://images.squarespace-cdn.com/content/v1/55240ca5e4b0ef65b027bcb8/1515442927108-1PC9EDB8U31MHWYZW52L/Girls+Rock+Lab_Page_1.png?format=750w" alt="girls rock logo" class="rounded mx-3" height="80" width="auto"></a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+    <div class="navbar-nav nav-menu">
+      <a class="nav-item nav-link" href="index.php">Home</a>
+      <a class="nav-item nav-link active" href="pepites.php">Les articles</a>
+      <a class="nav-item nav-link" href="category.php?category=oldies">Oldies</a>
+      <a class="nav-item nav-link" href="category.php?category=solo">Solo girls</a>
+    </div>
+  </div>
+</nav>
 
 <div class="header fixed-bg">
     <h1 class="ml-3">Girls Rock !</h1>
